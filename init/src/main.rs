858ff9cea91629d4ac1263a56228589d49b11f8d
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-12-05 21:02:38 +0100

    init: Refactor the run function to reduce indentation

--- init/src/main.rs
+++ init/src/main.rs
@@ -20,171 +20,166 @@ fn switch_stdio(stdio: &str) -> Result<()> {
 }
 
 pub fn run(file: &Path) -> Result<()> {
-    for line_raw in fs::read_to_string(file)?.lines() {
-        let line = line_raw.trim();
-        if !line.is_empty() && !line.starts_with('#') {
-            let mut args = line.split(' ').map(|arg| {
-                if arg.starts_with('$') {
-                    env::var(&arg[1..]).unwrap_or(String::new())
-                } else {
-                    arg.to_string()
+    for line in fs::read_to_string(file)?.lines() {
+        run_command(line);
+    }
+
+    Ok(())
+}
+
+fn run_command(line_raw: &str) {
+    let line = line_raw.trim();
+    if line.is_empty() || line.starts_with('#') {
+        return;
+    }
+    let mut args = line.split(' ').map(|arg| {
+        if arg.starts_with('$') {
+            env::var(&arg[1..]).unwrap_or(String::new())
+        } else {
+            arg.to_string()
+        }
+    });
+
+    if let Some(cmd) = args.next() {
+        match cmd.as_str() {
+            "cd" => {
+                let Some(dir) = args.next() else {
+                    println!("init: failed to cd: no argument");
+                    return;
+                };
+                if let Err(err) = env::set_current_dir(&dir) {
+                    println!("init: failed to cd to '{}': {}", dir, err);
                 }
-            });
+            }
+            "echo" => {
+                println!("{}", args.collect::<Vec<_>>().join(" "));
+            }
+            "export" => {
+                let Some(var) = args.next() else {
+                    println!("init: failed to export: no argument");
+                    return;
+                };
+                let mut value = String::new();
+                if let Some(arg) = args.next() {
+                    value.push_str(&arg);
+                }
+                for arg in args {
+                    value.push(' ');
+                    value.push_str(&arg);
+                }
+                unsafe { env::set_var(var, value) };
+            }
+            "run" => {
+                let Some(new_file) = args.next() else {
+                    println!("init: failed to run: no argument");
+                    return;
+                };
+                if let Err(err) = run(&Path::new(&new_file)) {
+                    println!("init: failed to run '{}': {}", new_file, err);
+                }
+            }
+            "run.d" => {
+                // This must be a BTreeMap to iterate in sorted order.
+                let mut entries = BTreeMap::new();
+                let mut missing_arg = true;
 
-            if let Some(cmd) = args.next() {
-                match cmd.as_str() {
-                    "cd" => {
-                        if let Some(dir) = args.next() {
-                            if let Err(err) = env::set_current_dir(&dir) {
-                                println!("init: failed to cd to '{}': {}", dir, err);
-                            }
-                        } else {
-                            println!("init: failed to cd: no argument");
-                        }
+                for new_dir in args {
+                    if !Path::new(&new_dir).exists() {
+                        // Skip non-existent dirs
+                        continue;
                     }
-                    "echo" => {
-                        if let Some(arg) = args.next() {
-                            print!("{}", arg);
-                        }
-                        for arg in args {
-                            print!(" {}", arg);
-                        }
-                        print!("\n");
-                    }
-                    "export" => {
-                        if let Some(var) = args.next() {
-                            let mut value = String::new();
-                            if let Some(arg) = args.next() {
-                                value.push_str(&arg);
-                            }
-                            for arg in args {
-                                value.push(' ');
-                                value.push_str(&arg);
-                            }
-                            env::set_var(var, value);
-                        } else {
-                            println!("init: failed to export: no argument");
-                        }
-                    }
-                    "run" => {
-                        if let Some(new_file) = args.next() {
-                            if let Err(err) = run(&Path::new(&new_file)) {
-                                println!("init: failed to run '{}': {}", new_file, err);
-                            }
-                        } else {
-                            println!("init: failed to run: no argument");
-                        }
-                    }
-                    "run.d" => {
-                        // This must be a BTreeMap to iterate in sorted order.
-                        let mut entries = BTreeMap::new();
-                        let mut missing_arg = true;
+                    missing_arg = false;
 
-                        for new_dir in args {
-                            if !Path::new(&new_dir).exists() {
-                                // Skip non-existent dirs
-                                continue;
-                            }
-                            missing_arg = false;
-
-                            match read_dir(&new_dir) {
-                                Ok(list) => {
-                                    for entry_res in list {
-                                        match entry_res {
-                                            Ok(entry) => {
-                                                // This intentionally overwrites older entries with
-                                                // the same filename to allow overriding entries in
-                                                // one search dir with those in a later search dir.
-                                                entries.insert(entry.file_name(), entry.path());
-                                            }
-                                            Err(err) => {
-                                                println!(
-                                                    "init: failed to run.d: '{}': {}",
-                                                    new_dir, err
-                                                );
-                                            }
-                                        }
-                                    }
-                                }
-                                Err(err) => {
-                                    println!("init: failed to run.d: '{}': {}", new_dir, err);
-                                }
-                            }
+                    let list = match read_dir(&new_dir) {
+                        Ok(list) => list,
+                        Err(err) => {
+                            println!("init: failed to run.d: '{}': {}", new_dir, err);
+                            continue;
                         }
-
-                        if missing_arg {
-                            println!(
-                                "init: failed to run.d: no argument or all dirs are non-existent"
-                            );
-                        } else {
-                            // This takes advantage of BTreeMap iterating in sorted order.
-                            for (_, entry_path) in entries {
-                                if let Err(err) = run(&entry_path) {
-                                    println!(
-                                        "init: failed to run '{}': {}",
-                                        entry_path.display(),
-                                        err
-                                    );
-                                }
+                    };
+                    for entry_res in list {
+                        match entry_res {
+                            Ok(entry) => {
+                                // This intentionally overwrites older entries with
+                                // the same filename to allow overriding entries in
+                                // one search dir with those in a later search dir.
+                                entries.insert(entry.file_name(), entry.path());
+                            }
+                            Err(err) => {
+                                println!("init: failed to run.d: '{}': {}", new_dir, err);
                             }
                         }
                     }
-                    "stdio" => {
-                        if let Some(stdio) = args.next() {
-                            if let Err(err) = switch_stdio(&stdio) {
-                                println!("init: failed to switch stdio to '{}': {}", stdio, err);
-                            }
-                        } else {
-                            println!("init: failed to set stdio: no argument");
+                }
+
+                if missing_arg {
+                    println!("init: failed to run.d: no argument or all dirs are non-existent");
+                    return;
+                }
+
+                // This takes advantage of BTreeMap iterating in sorted order.
+                for (_, entry_path) in entries {
+                    if let Err(err) = run(&entry_path) {
+                        println!("init: failed to run '{}': {}", entry_path.display(), err);
+                    }
+                }
+            }
+            "stdio" => {
+                let Some(stdio) = args.next() else {
+                    println!("init: failed to set stdio: no argument");
+                    return;
+                };
+                if let Err(err) = switch_stdio(&stdio) {
+                    println!("init: failed to switch stdio to '{}': {}", stdio, err);
+                }
+            }
+            "unset" => {
+                for arg in args {
+                    unsafe { env::remove_var(&arg) };
+                }
+            }
+            "nowait" => {
+                let Some(cmd) = args.next() else {
+                    println!("init: failed to run nowait: no argument");
+                    return;
+                };
+                let mut command = Command::new(cmd);
+
+                for arg in args {
+                    command.arg(arg);
+                }
+
+                match command.spawn() {
+                    Ok(_child) => {}
+                    Err(err) => println!("init: failed to execute '{}': {}", line, err),
+                }
+            }
+            _ => {
+                let mut command = Command::new(cmd.clone());
+                for arg in args {
+                    command.arg(arg);
+                }
+
+                let mut child = match command.spawn() {
+                    Ok(child) => child,
+                    Err(err) => {
+                        println!("init: failed to execute '{}': {}", line, err);
+                        return;
+                    }
+                };
+                match child.wait() {
+                    Ok(exit_status) => {
+                        if !exit_status.success() {
+                            println!("{cmd} failed with {exit_status}");
                         }
                     }
-                    "unset" => {
-                        for arg in args {
-                            env::remove_var(&arg);
-                        }
-                    }
-                    "nowait" => {
-                        if let Some(cmd) = args.next() {
-                            let mut command = Command::new(cmd);
-
-                            for arg in args {
-                                command.arg(arg);
-                            }
-
-                            match command.spawn() {
-                                Ok(_child) => {}
-                                Err(err) => println!("init: failed to execute '{}': {}", line, err),
-                            }
-                        } else {
-                            println!("init: failed to run nowait: no argument");
-                        }
-                    }
-                    _ => {
-                        let mut command = Command::new(cmd.clone());
-                        for arg in args {
-                            command.arg(arg);
-                        }
-
-                        match command.spawn() {
-                            Ok(mut child) => match child.wait() {
-                                Ok(exit_status) => {
-                                    if !exit_status.success() {
-                                        println!("{cmd} failed with {exit_status}");
-                                    }
-                                }
-                                Err(err) => {
-                                    println!("init: failed to wait for '{}': {}", line, err)
-                                }
-                            },
-                            Err(err) => println!("init: failed to execute '{}': {}", line, err),
-                        }
+                    Err(err) => {
+                        println!("init: failed to wait for '{}': {}", line, err)
                     }
                 }
             }
         }
     }
-
-    Ok(())
 }
 
 pub fn main() {
